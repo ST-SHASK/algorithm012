3c190f001d95634cd39d2b6b4a07a46ecdc1d344
suntao <st_shask@163.com>
2020-07-12 23:38:10 +0800

    week1 add content

--- Week_01/easy/RotateArray.java
+++ Week_01/easy/RotateArray.java
@@ -25,7 +25,36 @@ public class RotateArray {
     //链接：https://leetcode-cn.com/problems/rotate-array
     //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
-    public void rotate(int[] nums, int k) {
+    public static void rotate(int[] nums, int k) {
+        // 暴力
+        for (int i = 0; i < k; i++) {
+            int val = nums[nums.length - 1];
+            System.arraycopy(nums, 0, nums, 1, nums.length - 1);
+            nums[0] = val;
+        }
+    }
 
+    public static void rotate2(int[] nums, int k) {
+        k %= nums.length;
+        // 翻转 k 和 length - k 个元素
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private static void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[right];
+            nums[right--] = nums[left];
+            nums[left++] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2};
+        rotate2(nums, 3);
+        for (int i : nums) {
+            System.out.print(i);
+        }
     }
 }
